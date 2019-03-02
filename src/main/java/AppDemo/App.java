393255b96ef46523c4e16f0c7deaf28a03999ca5
unknown <dekx100@gmail.com>
2019-03-02 07:59:02 +0000

    commit

--- src/main/java/AppDemo/App.java
+++ src/main/java/AppDemo/App.java
@@ -1,13 +1,2 @@
 package AppDemo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
